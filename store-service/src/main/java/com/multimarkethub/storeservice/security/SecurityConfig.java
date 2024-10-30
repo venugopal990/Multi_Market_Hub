ee533b854377fce85bc6dfa1e6450465c57dc474
Venu Gopal Rao Dummu <venugopal.dummu@gmail.com>
2024-10-30 10:50:26 -0500

    Added Prometheus and Grafana for Monitoring the Application

--- store-service/src/main/java/com/multimarkethub/storeservice/security/SecurityConfig.java
+++ store-service/src/main/java/com/multimarkethub/storeservice/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated())
         .httpBasic().and()
         .csrf().disable(); 
 		
